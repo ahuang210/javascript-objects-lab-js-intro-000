bbbdaad03409d86d07353737232e29e69432254c
Ada Huang <ada10huang@gmail.com>
2020-08-11 23:48:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
 }
 
 var deleteFromObjectByKey = (object, key) => {
-  let object = Object.assign({}, object);
+  object = Object.assign({}, object);
   delete object[key];
   return object
 }
